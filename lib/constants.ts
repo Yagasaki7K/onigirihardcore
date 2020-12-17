a1f0c6639898fac7f09ecc574a3f821261db9e51
Anderson Marlon <anderson18_marlon@hotmail.com>
2020-12-17 20:33:57 -0300

    17/12 - Fix

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const EXAMPLE_PATH = 'blog-starter-typescript'
 export const CMS_NAME = 'Markdown'
 export const HOME_OG_IMAGE_URL =
-  'https://og-image.now.sh/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg'
+  'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/39583175853347.5c58b6ceee265.png'
